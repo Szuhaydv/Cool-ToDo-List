360fe9920914f57e841e5b19ef5ab095ed956dcb
szuhaydv <szuhaydv@gmail.com>
2024-02-06 09:59:45 +0100

    Tasks View UI Layout + some functionality

---
+++ Cool ToDo List/Components/CustomFilter.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFilter.swift
+//  Cool ToDo List
+//
+//  Created by Dávid Szuhay on 06/02/2024.
+//
+
+import SwiftUI
+
+struct CustomFilter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomFilter()
+}
---
+++ Cool ToDo List/TasksView.swift
@@ -0,0 +1,18 @@
+//
+//  TasksView.swift
+//  Cool ToDo List
+//
+//  Created by Dávid Szuhay on 06/02/2024.
+//
+
+import SwiftUI
+
+struct TasksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksView()
+}
